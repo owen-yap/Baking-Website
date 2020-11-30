d34e6df0a2f734f4bf155186ca44b0944120794a
Owen Yap <owen.y.yuhan@gmail.com>
2020-11-30 14:04:55 +0800

    Created products controller and added edit update and destroy actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   root to: 'pages#home'
+
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
