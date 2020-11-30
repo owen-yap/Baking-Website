e39ea2924723575608933653bf694ce797983125
Emily Chen <emchen02@gmail.com>
2020-11-30 15:28:03 +0800

    add user authenitction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parammeters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address])
+  end
+
 end
