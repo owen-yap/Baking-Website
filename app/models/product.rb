462aa090203f34591989101f5ab8ecaf9ef551b4
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-02 15:58:36 +0800

    Added dependent destroy to models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_one_attached :photo
   has_many :orders, dependent: :destroy
 end
