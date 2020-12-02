89255d2b38cda8045b9c8f59f04bb6a267eb545b
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-02 12:37:32 +0800

    Implemented pundit for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,27 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update destroy]
   skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @products = Product.all
+    @products = policy_scope(Product).order(created_at: :desc)
   end
 
   def my_products
     @products = current_user.products
+    skip_authorization
   end
 
   def new
     @product = Product.new
+    skip_authorization
   end
 
   def create
     @product = Product.new(product_params)
     @product.user = current_user
 
+    authorize @product
     if @product.save
       redirect_to product_path(@product)
     else
@@ -25,10 +30,11 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+    skip_authorization
   end
 
   def edit
+    authorize @product
   end
 
   def update
@@ -40,6 +46,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+    authorize @product
     @product.destroy
     redirect_to root_path
   end
