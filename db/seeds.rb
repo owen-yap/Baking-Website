64bae83262f4cf78591045309973b6e44359850b
Emily Chen <emchen02@gmail.com>
2020-12-02 12:55:44 +0800

    added seed for cupcake images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
+require "open-uri"
+
 puts "Clearing users..."
 User.destroy_all
 Product.destroy_all
+
+
+
 
 user = User.new(email: "abc@gmail.com", password: "123123")
 user.username = "JohnDoe"
 user.address = "International Towers, Orchard"
 user.save
 
+
+
 5.times do
   product = Product.new(name: "Cupcake", description: "Soft and fluffy", price: 15, category: "Pastry")
   product.user = user
+  file = URI.open('https://source.unsplash.com/300x400/?Cupcake')
+  product.photo.attach(io: file, filename: 'jpeg', content_type: 'image/jpeg')
   product.save
 end
+
+
