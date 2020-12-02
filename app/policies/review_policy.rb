245cfd912ea97b55c8bde9fb758bfdd0f058f609
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-02 15:09:16 +0800

    Added pundit for reviews

---
+++ app/policies/review_policy.rb
@@ -0,0 +1,16 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    raise
+    record.order.user == user && !record.order.review.nil?
+  end
+
+  def create?
+    record.order.user == user && Review.exists?(record.id) == false
+  end
+end
