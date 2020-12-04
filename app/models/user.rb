d833a9141b2b4f6274f8c7945de92901da47f8d9
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-04 11:59:03 +0800

    fixed issue with avatar default photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :products, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_one_attached :userphoto
+  has_many :reviews, through: :orders, dependent: :destroy
 
   def seller?
     !products.empty?
