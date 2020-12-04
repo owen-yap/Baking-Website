cd8d89c97df81c6906eda82c59cc0516f9f9b024
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-04 15:22:33 +0800

    fixed bugs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -33,9 +33,8 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initStarRating();
 });
 
 // app/javascript/packs/application.js
 import { initStarRating } from '../plugins/init_star_rating';
-
-initStarRating(); 
