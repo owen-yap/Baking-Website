4b74c5e9a820bb38ebca4b68bf4dda95316424dc
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-05 17:45:50 +0800

    added category models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_04_031434) do
+ActiveRecord::Schema.define(version: 2020_12_05_072457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 2020_12_04_031434) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -48,6 +54,15 @@ ActiveRecord::Schema.define(version: 2020_12_04_031434) do
     t.string "checkout_session_id"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "product_categories", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -90,6 +105,8 @@ ActiveRecord::Schema.define(version: 2020_12_04_031434) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
   add_foreign_key "products", "users"
   add_foreign_key "reviews", "orders"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,59 @@
 require "open-uri"
+require "faker"
 
 puts "Clearing database..."
 Review.destroy_all
 Order.destroy_all
 Product.destroy_all
 User.destroy_all
+Category.destroy_all
+
+# Categories
+categories = %w[breads cookies desserts cakes muffins sweets pastry rolls]
+categories.each { |category| Category.create(name: category) }
 
 # Seller
-seller = User.new(email: "abc@gmail.com", password: "123123")
+seller = User.new(email: "abc@gmail.com", password: "lewagon")
 seller.username = "John Doe"
 seller.address = "The Paterson, Singapore"
 seller.seller = true
+avatar_photo = URI.open("https://source.unsplash.com/300x400/?portrait")
+seller.userphoto.attach(io: avatar_photo, filename: 'jpeg', content_type: 'image/jpeg')
 seller.save!
 
-5.times do
-  product = Product.new(name: "Cupcake", description: "Soft and fluffy", price: (1..15).to_a.sample, category: "Pastry")
+seller2 = User.new(email: "123@gmail.com", password: "lewagon")
+seller2.username = "Alice bakes"
+seller2.address = "Orchard Towers, Singapore"
+seller2.seller = true
+avatar_photo = URI.open("https://source.unsplash.com/300x400/?portrait")
+seller2.userphoto.attach(io: avatar_photo, filename: 'jpeg', content_type: 'image/jpeg')
+seller2.save!
+
+2.times do
+  dessert = Faker::Dessert.variety
+  product = Product.new(name: "#{Faker::Dessert.flavor} #{dessert}", description: "Soft and fluffy", price: (1..15).to_a.sample, category: "Pastry")
   product.user = seller
-  file = URI.open('https://source.unsplash.com/300x400/?Cupcake')
+  product.category = Category.all.sample
+  file = URI.open("https://source.unsplash.com/300x400/?#{dessert}")
   product.photo.attach(io: file, filename: 'jpeg', content_type: 'image/jpeg')
   product.save
 end
 
+2.times do
+  dessert = Faker::Dessert.variety
+  product = Product.new(name: "#{Faker::Dessert.flavor} #{dessert}", description: "Soft and fluffy", price: (1..15).to_a.sample, category: "Pastry")
+  product.user = seller2
+  product.category = Category.all.sample
+  file = URI.open("https://source.unsplash.com/300x400/?#{dessert}")
+  product.photo.attach(io: file, filename: 'jpeg', content_type: 'image/jpeg')
+  product.save
+end
 #USer
 
-buyer = User.new(email: "buyer@gmail.com", password: "123456")
+buyer = User.new(email: "buyer@gmail.com", password: "lewagon")
 buyer.username = "Roger"
 buyer.address = "Ion Orchard, Singapore"
 buyer.save!
-
-
 
 product1 = Product.first
 
