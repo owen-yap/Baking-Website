4d004b98d92459eaceab5b8fe369fa87b4b0475a
Owen Yap <owen.y.yuhan@gmail.com>
2020-12-13 22:38:47 +0800

    view updated for delivery / self-collect logic

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,13 +28,34 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initStarRating } from '../plugins/init_star_rating';
+
+
+const deliveryRadio = document.getElementById('order_delivery_delivery');
+const selfCollectRadio = document.getElementById('order_delivery_self-collection');
+
+const deliveryDisplay = () => {
+  const selfCollectMsg = document.getElementById('self_collect_address');
+  const deliveryAddress = document.querySelector('.order_address');
+  if (deliveryRadio.checked) {
+    selfCollectMsg.classList.add("d-none");
+    deliveryAddress.classList.remove("d-none");
+  } else if (selfCollectRadio.checked) {
+    selfCollectMsg.classList.remove("d-none");
+    deliveryAddress.classList.add("d-none");
+  }
+}
+
+const isDelivery = () => {
+  deliveryRadio.addEventListener('click', deliveryDisplay);
+  selfCollectRadio.addEventListener('click', deliveryDisplay);
+}
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
   initStarRating();
+  deliveryDisplay();
+  isDelivery();
 });
-
-// app/javascript/packs/application.js
-import { initStarRating } from '../plugins/init_star_rating';
